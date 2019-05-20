9b4a271607b6bd4cd8be9162d7a0533eb8bd6934
Ninebell <ninebell94@gmail.com>
2019-05-20 13:30:29 +0900

    finish yolo data maker

--- data_maker.py
+++ data_maker.py
@@ -1,3 +1,5 @@
+import os
+import  glob
 import cv2
 import numpy as np
 
@@ -6,13 +8,18 @@ MAIN_WINDOW_NAME = "data_maker"
 img_list = []
 idx = 0
 
+label_path = "./dataset/label.txt"
+img_path = "./dataset/image"
+
 y_gap = 60
 
+
 class SaveInfo:
-    def __init__(self, img):
+    def __init__(self, img, img_name, bb_list=[]):
+        self.img_name = img_name
         self.origin_img = img
         self.edit_img = img.copy()
-        self.bound_list = []
+        self.bound_list = bb_list
 
     def add_bounding_box(self, bb):
         self.bound_list.append(bb)
@@ -24,6 +31,16 @@ class SaveInfo:
     def clear_bounding_box(self):
         self.bound_list.clear()
 
+    def set_bound_list(self, bb_list):
+        self.bound_list = bb_list
+
+    def __str__(self):
+        self.bound_list.sort(key=lambda bb:bb.label)
+        info = ""
+        for bb in self.bound_list:
+            info = info + "{}\n".format(bb)
+        return info
+
 
 class SaveInfoManager:
 
@@ -31,8 +48,8 @@ class SaveInfoManager:
     index = 0
 
     @staticmethod
-    def create_save_info(image):
-        SaveInfoManager.save_info_list.append(SaveInfo(image))
+    def create_save_info(image, label, bb_list):
+        SaveInfoManager.save_info_list.append(SaveInfo(image, label, bb_list))
 
     @staticmethod
     def get_save_info():
@@ -50,6 +67,18 @@ class SaveInfoManager:
     def set_save_info(idx):
         SaveInfoManager.index = idx % len(SaveInfoManager.save_info_list)
 
+    @staticmethod
+    def save_info():
+        save_list = SaveInfoManager.save_info_list
+
+        for save_info in save_list:
+            print("save_info: {}".format(save_info.img_name))
+            save_path = img_path +"/" + save_info.img_name
+            f = open(save_path+".txt", "w")
+            f.write(save_info.__str__())
+            print(save_info)
+            f.close()
+
 
 class BoundingBox:
     def __init__(self, x, y, w, h, label):
@@ -60,7 +89,7 @@ class BoundingBox:
         self.label = label
 
     def __str__(self):
-        return "label: {}, point({}, {}), width: {}, height: {}".format(self.label, self.x, self.y, self.w, self.h)
+        return "{:0.4f},{:.4f},{:.4f},{:.4f},{}".format(self.x, self.y, self.w, self.h,self.label)
 
 
 class BoundingBoxFactory:
@@ -68,21 +97,21 @@ class BoundingBoxFactory:
     pt2 = None
     label = 0
     label_limit = 0
-    label_dict = {}
+    label_list = []
 
     @staticmethod
-    def set_label_dict(label_dict):
-        BoundingBoxFactory.label_dict = label_dict
-        BoundingBoxFactory.label_limit = len(BoundingBoxFactory.label_dict)
-        print(len(label_dict))
+    def set_label_list(label_list):
+        BoundingBoxFactory.label_list = label_list
+        BoundingBoxFactory.label_limit = len(BoundingBoxFactory.label_list)
+        print(len(label_list))
 
     @staticmethod
     def get_label():
-        return BoundingBoxFactory.label_dict[BoundingBoxFactory.label]
+        return BoundingBoxFactory.label_list[BoundingBoxFactory.label]
 
     @staticmethod
     def change_label(up):
-        print("{}: {}".format(BoundingBoxFactory.label, BoundingBoxFactory.label_dict[BoundingBoxFactory.label]))
+        print("{}: {}".format(BoundingBoxFactory.label, BoundingBoxFactory.label_list[BoundingBoxFactory.label]))
         if up:
             BoundingBoxFactory.label = (BoundingBoxFactory.label + 1) % BoundingBoxFactory.label_limit
         else:
@@ -98,13 +127,19 @@ class BoundingBoxFactory:
             return BoundingBoxFactory.create_bb()
 
     @staticmethod
+    def clear_pts():
+        BoundingBoxFactory.pt1 = None
+        BoundingBoxFactory.pt2 = None
+
+    @staticmethod
     def create_bb():
+        height, width = SaveInfoManager.get_save_info().origin_img.shape[:2]
         pt1 = BoundingBoxFactory.pt1
         pt2 = BoundingBoxFactory.pt2
-        x = (pt1[0] + pt2[0])//2
-        y = (pt1[1] + pt2[1])//2
-        w = abs(pt2[0] - pt1[0])
-        h = abs(pt2[1] - pt1[1])
+        x = ((pt1[0] + pt2[0])//2)/width
+        y = ((pt1[1] + pt2[1])//2)/height
+        w = (abs(pt2[0] - pt1[0]))/width
+        h = (abs(pt2[1] - pt1[1]))/height
         BoundingBoxFactory.pt1 = None
         BoundingBoxFactory.pt2 = None
         return BoundingBox(x, y, w, h, BoundingBoxFactory.label)
@@ -130,6 +165,7 @@ def key_callback(key):
     if key == -1:
         return
 
+    BoundingBoxFactory.clear_pts()
     if '0' <= chr(key) <= '9':
         key_callback.number.append(chr(key))
 
@@ -138,31 +174,32 @@ def key_callback(key):
         SaveInfoManager.set_save_info(num)
 
     elif chr(key) == 'n':
-        SaveInfoManager.next_save_info()
         print("다음 이미지")
+        SaveInfoManager.next_save_info()
 
     elif chr(key) == 'b':
-        SaveInfoManager.get_save_info().remove_bounding_box()
         print("이전 박스 제거")
+        SaveInfoManager.get_save_info().remove_bounding_box()
 
     elif chr(key) == 's':
         print("현재 박스 저장")
+        SaveInfoManager.save_info()
 
     elif chr(key) == 'c':
-        SaveInfoManager.get_save_info().clear_bounding_box()
         print("박스 초기화")
+        SaveInfoManager.get_save_info().clear_bounding_box()
 
     elif chr(key) == 'p':
-        SaveInfoManager.prev_save_info()
         print("이전 이미지")
+        SaveInfoManager.prev_save_info()
 
     elif chr(key) == 'u':
-        BoundingBoxFactory.change_label(True)
         print("다음 라벨")
+        BoundingBoxFactory.change_label(True)
 
     elif chr(key) == 'd':
-        BoundingBoxFactory.change_label(False)
         print("이전 라벨")
+        BoundingBoxFactory.change_label(False)
 
     if not ('0' <= chr(key) <= '9'):
         key_callback.number.clear()
@@ -172,37 +209,84 @@ key_callback.number = []
 
 
 def draw_rectangle(image, bbox_list):
+    height, width = image.shape[:2]
     for box in bbox_list:
-        x = box.x
-        y = box.y
-        w = box.w
-        h = box.h
-        image = cv2.rectangle(image, (x - w//2, y - h//2), (x + w//2, y + h//2), (((box.label+1)*43)%255, 0, 0), 2)
+        x = int(box.x * width)
+        y = int(box.y * height)
+        w = int(box.w * width)
+        h = int(box.h * height)
+        color = (((box.label+1)*43)%255,((box.label+1)*53)%255,((box.label+1)*83)%255)
+        image = cv2.rectangle(image, (x - w//2, y - h//2), (x + w//2, y + h//2), color, 2)
     return image
-
 
 
 def make_print_img(print_img):
     height, width = print_img.shape[:2]
-    board = np.zeros([height+y_gap, width,3],dtype='uint8')
+    board = np.zeros([height+y_gap, width, 3], dtype='uint8')
     board = board + 255
-    cv2.line(board,(0, y_gap), (width,y_gap), (0,0,0), 2)
+    cv2.line(board, (0, y_gap), (width, y_gap), (0, 0, 0), 2)
     board = cv2.putText(board, BoundingBoxFactory.get_label(), (0, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
     print_img = draw_rectangle(print_img, SaveInfoManager.get_save_info().bound_list)
-    board[y_gap:y_gap+height, 0 :width] = print_img
+    board[y_gap:y_gap+height, 0:width] = print_img
 
     return board
 
 
+def get_image_name(img_path):
+    name_list = []
+    img_list = glob.glob(img_path+"/*.PNG")
+
+    print(img_path)
+    for name in img_list:
+        name = name.split("\\")[-1]
+        name = name.split(".")[0]
+        name_list.append(name)
+
+    return name_list
+
+
+def read_label_info(label_path):
+    f = open(label_path, "r")
+    label_list = []
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        line = line.replace("\n", "")
+        label_list.append(line)
+    return label_list
+
+
+def read_image(img_name):
+    img = cv2.imread(img_path+"/"+img_name+".PNG", cv2.IMREAD_COLOR)
+    return img
+
+
+def load_exist_labeling(img_name):
+    labels = []
+    f = open(img_path+"/"+img_name+".txt", "r")
+    bounding_list = []
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        line = line.replace("\n", "")
+        spec = line.split(",")
+        bb = BoundingBox(float(spec[0]), float(spec[1]), float(spec[2]), float(spec[3]), int(spec[4]))
+        bounding_list.append(bb)
+    return bounding_list
+
+
 if __name__ == "__main__":
 
-    label_dict = {0: "TEST", 1: "TEST2"}
-    BoundingBoxFactory.set_label_dict(label_dict)
-#   To-Do: 폴더에서 이미지 리스트 가져오기 (경로명)
-    img_path = ["../../test0.PNG", "../../test3.PNG"]
-    for path in img_path:
-        input_img = cv2.imread(path, cv2.IMREAD_COLOR)
-        SaveInfoManager.create_save_info(input_img)
+    label_list = read_label_info(label_path)
+    BoundingBoxFactory.set_label_list(label_list)
+    img_name_list = get_image_name(img_path)
+
+    for i in range(0, len(img_name_list)):
+        input_img = read_image(img_name_list[i])
+        bb_list = load_exist_labeling(img_name_list[i])
+        info = SaveInfoManager.create_save_info(input_img, img_name_list[i], bb_list)
 
     img_list.append(input_img)
     cv2.namedWindow(MAIN_WINDOW_NAME)
@@ -210,7 +294,6 @@ if __name__ == "__main__":
     key = 0
     while key != 27:
         key = cv2.waitKey(1)
-
         print_img = make_print_img(SaveInfoManager.get_save_info().edit_img.copy())
         cv2.imshow(MAIN_WINDOW_NAME, print_img)
         if key == -1:
